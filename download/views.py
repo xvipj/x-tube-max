9c41a61f55e67cd305064b0d11928764b1065f9e
xavier <xavierperezjimenez4@gmail.com>
2024-03-30 10:25:17 -0500

    mp4 a mp3 en audio

--- download/views.py
+++ download/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from pytube import YouTube
+import os
+import moviepy.editor as mp
 
 # Create your views here.
 def download_video(request):
@@ -24,10 +26,21 @@ def download_audio(request):
         enlace_video = request.POST['enlace_audio']
         ruta_descarga = 'audio/'  # Ajusta según tu configuración
         try:
+            # Descargar el video de YouTube
             youtube = YouTube(enlace_video)
             stream = youtube.streams.filter(only_audio=True).first() 
-            stream.download(ruta_descarga)
-            mensaje = "¡Audio descargado correctamente!"
+            video_path = stream.download(output_path=ruta_descarga)
+
+            # Convertir el archivo de video a MP3
+            audio_path = os.path.splitext(video_path)[0] + '.mp3'
+            clip = mp.AudioFileClip(video_path)
+            clip.write_audiofile(audio_path)
+            clip.close()
+
+            # Eliminar el archivo de video temporal
+            os.remove(video_path)
+
+            mensaje = "¡Audio descargado correctamente como MP3!"
         except Exception:
             mensaje = "Ingrese una URL válida"
 
