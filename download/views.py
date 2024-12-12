fa43051dc550ace8b4818d90a065cb2f0052a578
xavier <xavierperezjimenez4@gmail.com>
2024-12-12 14:10:10 -0500

    v1.1.1
    download playlist audio

--- download/views.py
+++ download/views.py
@@ -19,7 +19,6 @@ def download_video(request):
 
     return render(request, 'video.html', {'mensaje': mensaje})
 
-
 def download_audio(request):
     mensaje = ""  
     if request.method == 'POST':
@@ -47,7 +46,7 @@ def download_audio(request):
     return render(request, 'audio.html', {'mensaje': mensaje})
 
 
-def download_video_playlist(request):
+def download_video_playlist(request): 
     if request.method == 'POST':
         playlist_url = request.POST.get('enlace_playlist')
         ruta_descarga = 'playlist/' 
@@ -74,4 +73,35 @@ def download_video_playlist(request):
     return render(request, 'video_playlist.html')
 
 def download_audio_playlist(request):
+    if request.method == 'POST':
+        playlist_url = request.POST.get('enlace_playlist')
+        ruta_descarga = 'playlist/'
+        
+        try:
+            # Crear objeto Playlist
+            playlist = Playlist(playlist_url)
+            
+            # Recorrer cada video y descargar el stream progresivo más adecuado
+            for musica in playlist.videos:
+                try:
+                    # Obtener un stream progresivo con video y audio
+                    stream = musica.streams.filter(progressive=True).get_highest_resolution()
+                    video_path = stream.download(output_path=ruta_descarga)
+
+                    # Convertir el archivo de video a MP3
+                    audio_path = os.path.splitext(video_path)[0] + '.mp3'
+                    clip = mp.AudioFileClip(video_path)
+                    clip.write_audiofile(audio_path)
+                    clip.close()
+
+                    # Eliminar el archivo de video temporal
+                    os.remove(video_path)
+                except Exception as video_error:
+                    print(f"Error al descargar el video {musica.title}: {video_error}")
+            
+            # Mostrar el resultado en la página
+            return render(request, 'audios_playlist.html', {'mensaje': '¡Playlist descargada con éxito!'})
+
+        except Exception as e:
+            return render(request, 'audios_playlist.html', {'mensaje': f"Error al procesar la playlist: {str(e)}"})
     return render(request, 'audios_playlist.html')
